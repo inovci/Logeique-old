8f982efd525f75ecacd3086ad3721103c712a9d9
GUEI Deoth <gueideoth@gmail.com>
2021-07-10 21:02:29 +0200

    pycache

--- chat/models.py
+++ chat/models.py
@@ -4,12 +4,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Room(models.Model):
-
-=======
     user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user1_rooms")
     user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user2_rooms")
     #code = models.CharField(unique = True , max_length = 250 )
->>>>>>> 0db2d56aa564a8a112f31fd169cb36ed2c269b1e
 
 class Message(models.Model):
     value = models.TextField(max_length=100000, blank=True)
