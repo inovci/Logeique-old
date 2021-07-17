332d10d5fc7c1b1309f035a125f455f26535705e
GUEI Deoth <gueideoth@gmail.com>
2021-07-17 16:24:54 +0200

    chat paths update

--- chat/urls.py
+++ chat/urls.py
@@ -5,4 +5,5 @@ app_name = "chat"
 
 urlpatterns = [
 	path('user/<int:user1_id>/user/<int:user2_id>', views.checkview, name='checkview'),
+	path('send/<int:user_id>/<int:room_id>' , views.send , name = 'send')
 ]
