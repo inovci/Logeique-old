2b8782019c5ccf215dc1403c7d6066aca9ef5eac
Boty Javan <inovci2@gmail.com>
2021-07-22 17:19:42 +0000

    Update chat design and display user image

--- chat/urls.py
+++ chat/urls.py
@@ -5,9 +5,7 @@ app_name = "chat"
 
 urlpatterns = [
 	path('<str:other_user>', views.checkview, name='checkview'),
-	#path('<str:user1>/<str:user2>', views.clientCheckview, name='clientCheckview'),
 	path('send/<int:user_id>/<int:room_id>', views.send, name='send'),
 	path('room/<int:room_id>', views.room, name='room'),
 	path('receive/<int:room_id>' , views.receive , name = 'receive'),
-	#re_path(r"^(?P<username>[\w.@+-]+)", ThreadView.as_view()),
 ]
--- chat/views.py
+++ chat/views.py
@@ -5,8 +5,6 @@ from django.contrib.auth.models import User
 from django.db.models import Q
  
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
 
 def room(request, room_id):
     room = Room.objects.get(id=room_id)
