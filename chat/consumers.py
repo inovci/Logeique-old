8d56c06319727718111e31e6ae3849faea8afcca
Boty Javan <inovci2@gmail.com>
2021-07-22 16:19:51 +0000

    Add momentjs and fix each incomming message from a user

--- chat/consumers.py
+++ chat/consumers.py
@@ -39,7 +39,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         if front_text is not None:
             loaded_dict_data = json.loads(front_text)
             msg = loaded_dict_data.get('message')
-            print(msg)
             user = self.scope['user']
             username = 'default_user'
             if user.is_authenticated:
@@ -83,47 +82,3 @@ class ChatConsumer(AsyncWebsocketConsumer):
     def creat_chat_message(self, user, msg):
         room_obj = self.room_obj
         return Message.objects.create(room=room_obj, user=user, value=msg)
-
-
-class ChatConsumerTuto(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
-
-        # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
-
-    # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
