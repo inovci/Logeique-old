3b1821f1a6582f93026532ff599fc023514283ee
Boty Javan <inovci2@gmail.com>
2021-07-24 11:14:37 +0000

    Remove send and receive

--- chat/urls.py
+++ chat/urls.py
@@ -5,7 +5,5 @@ app_name = "chat"
 
 urlpatterns = [
 	path('<str:other_user>', views.checkview, name='checkview'),
-	path('send/<int:user_id>/<int:room_id>', views.send, name='send'),
 	path('room/<int:room_id>', views.room, name='room'),
-	path('receive/<int:room_id>' , views.receive , name = 'receive'),
 ]
