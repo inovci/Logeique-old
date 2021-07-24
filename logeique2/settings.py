a676dc52ec9aec36618b83f848ac0357fdd4c683
Boty Javan <inovci2@gmail.com>
2021-07-24 14:26:56 +0000

    Repush

--- logeique2/settings.py
+++ logeique2/settings.py
@@ -97,7 +97,9 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': 'localhost',
         'OPTIONS':{
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+            'use_unicode': True,
         }
     }
 }
