bb5f721cb00e3fc192a649ecf39ebf357142099e
Deoth <gueideoth@gmail.com>
2021-09-14 02:33:26 +0000

    Modifying templates

--- chat/urls.py
+++ chat/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
 	path('<int:house_id>', views.checkviewClient, name='checkviewClient'),
 	path('<str:other_user>-<int:proposal_id>', views.checkviewLandlord, name='checkviewLandlord'),
 	path('room/<int:room_id>', views.room, name='room'),
-	path('room/<int:lanlord_id>-<int:client_id>-<int:house_id>/' , views.concluded_deal , name='concluded_deal')
 ]
