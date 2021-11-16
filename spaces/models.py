11515a09fc2ab080c278526af1ff9129909312ea
Boty Javan N'GUESSAN <inovci2@gmail.com>
2021-11-16 11:30:43 +0000

    Add default attribute for avatar var

--- spaces/models.py
+++ spaces/models.py
@@ -6,10 +6,13 @@ from chat.models import Room
 
 # Create your models here.
 
+
 class Client(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='client')
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='client')
     contact = models.CharField(max_length=50)
-    avatar = models.ImageField(null=True, blank=True, upload_to="img/avatars/")
+    avatar = models.ImageField(
+        null=True, blank=True, upload_to="img/avatars/", default="img/avatars/default-user-img.png")
 
     class Meta():
         ordering = ['user', 'contact']
@@ -17,32 +20,38 @@ class Client(models.Model):
     def __str__(self):
         return f"{self.user} - {self.contact}"
 
+
 class Proposal(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="proposals")
+    client = models.ForeignKey(
+        Client, on_delete=models.CASCADE, related_name="proposals")
     rent_proposal = models.BigIntegerField(null=True)
     deposit_proposal = models.BigIntegerField(null=True)
     kind_desire = models.CharField(max_length=50, null=True, default=None)
     rooms_number_desire = models.IntegerField(null=True, default=None)
-    area_desire = models.CharField(max_length=50 ,null=True, default=None)
-    township_desire = models.CharField(max_length=50 ,null=True, default=None)
-    
+    area_desire = models.CharField(max_length=50, null=True, default=None)
+    township_desire = models.CharField(max_length=50, null=True, default=None)
+
     class Meta():
         ordering = ['client', 'rent_proposal', 'deposit_proposal']
 
     def __str__(self):
         return f"{self.id} - {self.client}"
 
+
 class Landlord(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='landlord')
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='landlord')
     contact = models.CharField(max_length=50)
     avatar = models.ImageField(null=True, blank=True, upload_to="img/avatars/")
-    clients = models.ManyToManyField(Client, related_name="landlords", through="Deal")
+    clients = models.ManyToManyField(
+        Client, related_name="landlords", through="Deal")
 
     class Meta():
         ordering = ['user', 'contact']
 
     def __str__(self):
         return f"{self.user} - {self.contact}"
+
 
 class House(models.Model):
     house_township = models.CharField(max_length=50)
@@ -55,13 +64,16 @@ class House(models.Model):
     house_to_sell = models.BooleanField()
     house_image = models.ImageField(upload_to="img/houses/")
     house_creation_day = models.DateTimeField(default=datetime.now)
-    landlord = models.ForeignKey(Landlord, on_delete=models.CASCADE, related_name="houses")
+    landlord = models.ForeignKey(
+        Landlord, on_delete=models.CASCADE, related_name="houses")
 
     class Meta():
-        ordering = ['house_township', 'house_area', 'house_kind', 'house_rooms_number', 'house_rent', 'house_deposit']
+        ordering = ['house_township', 'house_area', 'house_kind',
+                    'house_rooms_number', 'house_rent', 'house_deposit']
 
     def __str__(self):
         return f"{self.id} - {self.landlord}"
+
 
 class Deal(models.Model):
     client = models.ForeignKey(
@@ -75,13 +87,13 @@ class Deal(models.Model):
         related_name="landlord_deals"
     )
     house = models.ForeignKey(
-        House, 
+        House,
         on_delete=models.CASCADE,
         related_name="house_deals",
         default=None
     )
     room = models.ForeignKey(
-        Room, 
+        Room,
         on_delete=models.CASCADE,
         related_name="room_deals",
         default=None
@@ -100,29 +112,26 @@ class ClientFile(models.Model):
     )
 
     house = models.OneToOneField(
-        House , 
+        House,
         on_delete=models.CASCADE,
-        primary_key= True,
+        primary_key=True,
         related_name="house_client_files"
     )
-    
+
     id_card = models.ImageField(upload_to="img/id_cards/")
 
-    payslip1 = models.ImageField(upload_to = "img/payslips/")
-    payslip1 = models.ImageField(upload_to = "img/payslips/")
-    payslip1 = models.ImageField(upload_to = "img/payslips/") 
+    payslip1 = models.ImageField(upload_to="img/payslips/")
+    payslip1 = models.ImageField(upload_to="img/payslips/")
+    payslip1 = models.ImageField(upload_to="img/payslips/")
 
-    energy_bill1 = models.ImageField(upload_to = "img/bills/")
-    energy_bill2 = models.ImageField(upload_to = "img/bills/")
+    energy_bill1 = models.ImageField(upload_to="img/bills/")
+    energy_bill2 = models.ImageField(upload_to="img/bills/")
 
-    last_rent_receipt1 = models.ImageField(upload_to = "img/last_rent_receipts/")
-    last_rent_receipt2 = models.ImageField(upload_to = "img/last_rent_receipts/")
+    last_rent_receipt1 = models.ImageField(upload_to="img/last_rent_receipts/")
+    last_rent_receipt2 = models.ImageField(upload_to="img/last_rent_receipts/")
 
     occupancy_date = models.DateField()
 
     balance = models.BigIntegerField()
 
     departure_date = models.DateField(default=datetime.now)
-
-
-
